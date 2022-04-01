cb461660f9c395475a073c713ef482b24c55039d
prinzderick <54482059+prinzderick@users.noreply.github.com>
2022-04-01 07:59:21 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$lower_chars = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+
+$upper_chars = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+
+$numbers = array('0','1','2','3','4','5','6','7','8','9');
+
+$special_chars = array('!','@','#','$','%','&','*','_');
+
+
+function generate_base(){
+  $comb = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  $shfl = str_shuffle($comb);
+  $lenght = rand(5,10);
+  $base = substr($shfl,0,$lenght);
+  return $base;
+}
+
+function generate_password($base,$lower_chars,$upper_chars,$numbers,$special_chars){
+  
+}
+
+?>
